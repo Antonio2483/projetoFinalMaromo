ab1d40a1af35febf21e76737ec96561536f736f2
Antonio2483 <antoniolsferreira2015@gmail.com>
2023-10-30 21:31:01 -0300

    classe veiculo

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,2 +1,29 @@
 public abstract class Veiculo {
+    private String marca;
+    private String modelo;
+    private double preco;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
